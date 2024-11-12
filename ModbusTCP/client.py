4b38869ea7524bca7e62c53277bdf45894a25059
a.fifflaren <add.koshelev@gmail.com>
2024-11-12 20:33:14 +0300

    fix error for "---"&delete scada_passing

--- ModbusTCP/client.py
+++ ModbusTCP/client.py
@@ -1,96 +1,129 @@
 import os
 import logging
+import struct
 from time import sleep
 from dotenv import load_dotenv
 from pymodbus.client import ModbusTcpClient
-from scada_passing import driver, url_address, sign_in, to_iframe, query_from_table
-import struct
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
+# Загрузка переменных окружения
+load_dotenv()
+
+class WebDriver:
+    def __init__(self):
+        self.driver = self.create_driver()
+
+    @staticmethod
+    def create_driver():
+        options = Options()
+        options.headless = True
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    def url_address(self, url):
+        self.driver.get(url)
+
+    def sign_in(self, login, password):
+        self._enter_text(By.ID, "txtUsername", login)
+        self._enter_text(By.ID, "txtPassword", password, press_enter=True)
+
+    def _enter_text(self, by, identifier, text, press_enter=False):
+        element = self.driver.find_element(by, identifier)
+        element.clear()
+        element.send_keys(text)
+        if press_enter:
+            element.send_keys(Keys.RETURN)
+
+    def to_iframe(self, idFrame):
+        iframe = self.driver.find_element(By.ID, idFrame)
+        self.driver.switch_to.frame(iframe)
+
+    def query_from_table(self, trData):
+        return self.driver.find_element(By.XPATH, trData).text
+
+    def close(self):
+        self.driver.quit()
+
+class ModbusClient:
+    def __init__(self, host, port):
+        self.client = ModbusTcpClient(host, port=port)
+        self.connect()
+
+    def connect(self):
+        return self.client.connect()
+
+    def read_holding_registers(self, address, count):
+        return self.client.read_holding_registers(address, count)
+
+    def write_registers(self, address, registers):
+        return self.client.write_registers(address, registers)
+
+    def close(self):
+        self.client.close()
 
 def main():
     print(f'Запускается парсер!')
+    web_driver = WebDriver()
+    modbus_client = ModbusClient('localhost', os.getenv('port_client'))
 
-    url_address(url= os.getenv('url_scada'))
+    try:
+        web_driver.url_address(url=os.getenv('url_scada'))
+        web_driver.sign_in(login=os.getenv('login'), password=os.getenv('password'))
+        sleep(3)
+        web_driver.to_iframe(idFrame="frameView")
 
-    sign_in(login = os.getenv('login'), password = os.getenv('password'))
+        while True:
+            result = modbus_client.read_holding_registers(1, 16)
+            if not result.isError():
+                print(f"Регистры хранения: {result.registers}")
+            else:
+                print("Ошибка чтения регистров хранения")
 
-    sleep (3) #Пауза в 3 секунды
+            float_scada = []
 
-    to_iframe(idFrame="frameView")
+            for i in range(1, 17):
+                tr_data = f"//table/tbody/tr[{i}]/td[2]"
+                float_value = web_driver.query_from_table(trData=tr_data)
+                float_value = float_value.replace(" ", "").replace(",", ".")
 
-
-
-    # Получение данных из .env файла
-    load_dotenv()
-
-    # Настройка логирования
-    # logging.basicConfig()
-    # log = logging.getLogger()
-    # log.setLevel(logging.DEBUG)
-
-    # Настройка клиента
-    client = ModbusTcpClient('localhost', port = os.getenv('port_client'))
-
-    # Подключение к шлюзу
-    if client.connect():
-        try:
-            while True:
-                # Чтение 10 регистров хранения начиная с адреса 0
-                result = client.read_holding_registers(1, 16)
-                if not result.isError():
-                    print(f"Регистры хранения: {result.registers}")
-                else:
-                     print("Ошибка чтения регистров хранения")
-                
-                # Получения значений с таблицы в массив для хранения значений float
-                float_scada = []
-
-                # Цикл для получения значений из таблицы
-                for i in range(1, 17):  # от 1 до 17
-                    tr_data = f"//table/tbody/tr[{i}]/td[2]"
-                    float_value = query_from_table(trData=tr_data)
-                    float_scada.append(float_value.replace(" ", "").replace(",", "."))  # Добавление значения в список
-
-
-                # Проверка на допустимость значения
-                if float_value and float_value != "---":  # Если значение не пустое и не "---"
+                if float_value not in {None, "", "---"}:
                     try:
-                        float_value = float(float_value)  # Преобразование в float
-                        float_scada.append(float_value)  # Добавление значения в список
+                        # Если value - число, оставляем его, иначе обрабатываем
+                        if isinstance(float_value, (int, float)):
+                            float_value = float(float_value)
+                        else:
+                            float_value = float(float(float_value.replace(" ", "").replace(",", ".")))
                     except ValueError:
-                        print(f"Недопустимое значение: {float_value}")  # Логирование ошибки
-                        float_scada.append(0)  # Например, добавляем None для недопустимого значения
+                        print(f"Недопустимое значение: {float_value}")
+                        float_value = 0
                 else:
-                    float_scada.append(0)  # Или просто None если значение "---"
-                
-                # Вывод значений из float_scada для проверки
-                print(float_scada)
+                    float_value = 0
 
-                # Цикл для записи значений в регистры
-                for i in range(len(float_scada)):
-                    # Преобразование float значения в два 16-битных регистра в формате little-endian
-                    float_value = float(float_scada[i])  # Преобразуем строку в float
-                    packed_value = struct.pack('<f', float_value)  # '<f' означает little-endian формат для float
-                    registers = struct.unpack('<H', packed_value[2:])[0]  # unpack в 2 16-битных регистра, выбран только 1 их них (старший)
+                float_scada.append(float_value)
 
-                    # Запись в регистры
-                    result = client.write_registers(i + 1, registers)
+            print(float_scada)
 
-                    # Проверка результата записи
-                    if result.isError():
-                        print(f"Ошибка записи для регистра {i + 1}: {result}")
-                    else:
-                        print(f"Запись выполнена успешно в регистр {i + 1}!")
+            for i, value in enumerate(float_scada):
+                packed_value = struct.pack('<f', value)
+                registers = struct.unpack('<H', packed_value[2:])[0] 
+                result = modbus_client.write_registers(i + 1, registers)
 
-                #Задержка в 2 секунды
-                sleep(2)
+                if result.isError():
+                    print(f"Ошибка записи для регистра {i + 1}: {result}")
+                else:
+                    print(f"Запись выполнена успешно в регистр {i + 1}!")
 
-        finally:
-            # Закрытие соединения
-            client.close()
-    else:
-        print("Не удалось подключиться к шлюзу")
-    
+            sleep(2)
+
+    finally:
+        web_driver.close()
+        modbus_client.close()
 
 if __name__ == "__main__":
     main()
